89a6cf34c96fe4289af256e965429bf0fa57ebda
Canh Ngo <c.ngo@onehippo.com>
2016-04-15 14:24:18 +0200

    Created user manager to authenticate user based on his email & google authentication token

--- google-auth/src/main/java/org/onehippo/forge/googleauth/repository/GoogleSignInUserManager.java
+++ google-auth/src/main/java/org/onehippo/forge/googleauth/repository/GoogleSignInUserManager.java
@@ -19,8 +19,12 @@ package org.onehippo.forge.googleauth.repository;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
 import javax.jcr.SimpleCredentials;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryResult;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
@@ -28,28 +32,141 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 
-import org.hippoecm.repository.security.user.RepositoryUserManager;
+
+import org.apache.commons.lang.StringUtils;
+import org.hippoecm.repository.api.HippoNodeType;
+import org.hippoecm.repository.util.JcrUtils;
 import org.onehippo.forge.googleauth.GHippoCredential;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-public class GoogleSignInUserManager extends RepositoryUserManager {
+public class GoogleSignInUserManager extends AbstractRepositoryUserManager {
     private static final Logger log = LoggerFactory.getLogger(GoogleSignInUserManager.class);
+    public static final String PROP_EMAIL = "hipposys:email";
 
     private GoogleIdTokenVerifier gVerifier;
 
     @Override
-    public boolean authenticate(final SimpleCredentials creds) throws RepositoryException {
-        final Object idToken = creds.getAttribute(GHippoCredential.ATTRIBUTE_IDTOKEN);
-        if (idToken != null) {
-            return authenticate((String) idToken);
-        }
-
-        return super.authenticate(creds);
+    public boolean hasUser(final String userEmailId) throws RepositoryException {
+        final Node userNode = getUser(userEmailId);
+        return userNode != null;
     }
 
-    private boolean authenticate(String googleIdToken) throws RepositoryException {
+    /**
+     * Check if the given node contains a property 'PROP_EMAIL' with value in <code>expectedUserEmailId</code>
+     */
+    private static boolean hasEmail(final Node userNode, final String expectedUserEmailId) {
+        try {
+            if (userNode == null) {
+                log.error("User node is null");
+                return false;
+            }
+            return StringUtils.equals(expectedUserEmailId, JcrUtils.getStringProperty(userNode, PROP_EMAIL, StringUtils.EMPTY));
+        } catch (RepositoryException e) {
+            log.warn("Failed to query email", e);
+        }
+        return false;
+    }
+
+    private String getUserRootPath() {
+        return "/hippo:configuration/hippo:users";
+    }
+
+    /**
+     * Return the node having email address as the userId
+     * @param userEmailId  the google email address identifying the user
+     * @return
+     * @throws RepositoryException
+     */
+    @Override
+    public Node getUser(final String userEmailId) throws RepositoryException {
+        final String rootUserPath = getUserRootPath();
+        if (session.getRootNode().hasNode(rootUserPath)) {
+            final Node rootUsersNode = session.getRootNode().getNode(rootUserPath);
+            final NodeIterator userNodes = rootUsersNode.getNodes();
+            while (userNodes.hasNext()) {
+                final Node userNode = userNodes.nextNode();
+                if (userNode.getPrimaryNodeType().isNodeType(HippoNodeType.NT_USER) && hasEmail(userNode, userEmailId)) {
+                    return userNode;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Node createUser(final String userId) throws RepositoryException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public NodeIterator listUsers(final long offset, final long limit) throws RepositoryException {
+       return listUsers(null, offset, limit);
+    }
+
+    @Override
+    public NodeIterator listUsers(final String providerId, final long offset, final long limit) throws RepositoryException {
+        StringBuilder statement = new StringBuilder();
+        statement.append("//element");
+        statement.append("(*, ").append(HippoNodeType.NT_USER).append(")");
+        if (providerId != null) {
+            statement.append('[');
+            statement.append("@");
+            statement.append(HippoNodeType.HIPPO_SECURITYPROVIDER).append("= '").append(providerId).append("'");
+            statement.append(']');
+        }
+        statement.append(" order by @jcr:name");
+
+        Query q = session.getWorkspace().getQueryManager().createQuery(statement.toString(), Query.XPATH);
+        if (offset > 0) {
+            q.setOffset(offset);
+        }
+        if (limit > 0) {
+            q.setLimit(limit);
+        }
+        QueryResult result = q.execute();
+        return result.getNodes();
+    }
+
+    @Override
+    public boolean isActive(final String userEmailId) throws RepositoryException {
+        return JcrUtils.getBooleanProperty(getUser(userEmailId), HippoNodeType.HIPPO_ACTIVE, true);
+    }
+
+    @Override
+    public boolean isPasswordExpired(final String userId) throws RepositoryException {
+        return false;
+    }
+
+    @Override
+    public void syncUserInfo(final String userId) {
+    }
+
+    @Override
+    public void updateLastLogin(final String userId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void saveUsers() throws RepositoryException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean authenticate(final SimpleCredentials creds) throws RepositoryException {
+        if (!hasUser(creds.getUserID())) {
+            return false;
+        }
+
+        final Object idToken = creds.getAttribute(GHippoCredential.ATTRIBUTE_IDTOKEN);
+        if (idToken == null || !(idToken instanceof String)) {
+            return false;
+        }
+
+        return verifyToken((String)idToken);
+    }
+
+    private boolean verifyToken(String googleIdToken) throws RepositoryException {
         log.debug("Authenticating with Google OAuth2");
         if (gVerifier == null) {
             NetHttpTransport transport = new NetHttpTransport();
