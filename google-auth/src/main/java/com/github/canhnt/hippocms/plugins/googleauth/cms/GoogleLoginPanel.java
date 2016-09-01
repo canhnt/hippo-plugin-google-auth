209ea7afff05163dccd333334ee32031392d41c6
Canh Ngo <c.ngo@onehippo.com>
2016-09-01 17:02:42 +0200

    Change project package naming to com.github.canhnt.hippocms.plugins:hippocms-plugin-google-auth

--- google-auth/src/main/java/com/github/canhnt/hippocms/plugins/googleauth/cms/GoogleLoginPanel.java
+++ google-auth/src/main/java/com/github/canhnt/hippocms/plugins/googleauth/cms/GoogleLoginPanel.java
@@ -5,16 +5,17 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
-package org.onehippo.forge.googleauth.cms;
+package com.github.canhnt.hippocms.plugins.googleauth.cms;
 
 import java.security.AccessControlException;
 import java.util.HashMap;
@@ -24,6 +25,8 @@ import java.util.Map;
 
 import javax.jcr.SimpleCredentials;
 import javax.servlet.http.HttpSession;
+
+import com.github.canhnt.hippocms.plugins.googleauth.GHippoCredential;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
@@ -44,7 +47,6 @@ import org.hippoecm.frontend.plugins.login.ExtendConcurentLoginFilter;
 import org.hippoecm.frontend.session.LoginException;
 import org.hippoecm.frontend.session.PluginUserSession;
 import org.hippoecm.frontend.util.WebApplicationHelper;
-import org.onehippo.forge.googleauth.GHippoCredential;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
